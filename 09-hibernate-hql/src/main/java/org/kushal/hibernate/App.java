5cc8900f0aa194723103c0ad82e9635ecd0baaae
Kushal <52573092+KushalK2507@users.noreply.github.com>
2024-10-11 22:54:19 +0530

    Converting Project to Maven Project

--- 09-hibernate-hql/src/main/java/org/kushal/hibernate/App.java
+++ 09-hibernate-hql/src/main/java/org/kushal/hibernate/App.java
@@ -25,7 +25,7 @@ public class App {
     try {
       session.beginTransaction();
       // Fetching all records
-      Query query = session.createQuery("from users");
+      Query<Users> query = session.createQuery("from users",Users.class);
       query.setFirstResult(
           4); // It skips first 4 rows and fetches the record from table and fetches the
       // records after the 4th row.
@@ -37,22 +37,22 @@ public class App {
       // any parameter in @Entity then here we
       // will use the name of the class(Entity class)
 
-      listUsers.forEach(i -> System.out.println(i));
+      listUsers.forEach(System.out::println);
       System.out.println("*****************");
 
-      Query query1 =
+      Query<String> query1 =
           session.createQuery(
-              "select userdId from users"); // We can also use the slect in HQL to get
+              "select userdId from users",String.class); // We can also use the slect in HQL to get
       // the specific column instrad of
       // getting the the whole object.
       // In this we need to make sure we pass the dataMember but not the column name
-      List<String> userIds = (List<String>) query1.getResultList();
-      userIds.forEach(j -> System.out.println(j));
+      List<String> userIds = query1.getResultList();
+      userIds.forEach(System.out::println);
 
       // Parameter in HQL.
       int userId = 0;
       String userName = "Kushal";
-      Query query2 = session.createQuery("from users where userId > ? and userName = ?");
+      Query<Users> query2 = session.createQuery("from users where userId > ? and userName = ?",Users.class);
       query2.setParameter(0, userId);
       query2.setParameter(1, userName);
       // In above the HQL ? is a place Holder in Query and query.setInteger Or
@@ -60,16 +60,16 @@ public class App {
 
       int userId1 = 0;
       String userName1 = "Kushal";
-      Query query3 =
-          session.createQuery("from users where userId > :userId and userName = :userName1");
+      Query<Users> query3 =
+          session.createQuery("from users where userId > :userId and userName = :userName1",Users.class);
       query3.setParameter("userId", userId);
       query3.setParameter("userName1", userName);
       // Instrad of giving the position we can give the name of the Property.
 
       // Use of where clause
       List<Users> listUser =
-          session.createQuery("from users where username='Hello'").getResultList();
-      listUser.forEach(z -> System.out.println(z));
+          session.createQuery("from users where username='Hello'",Users.class).getResultList();
+      listUser.forEach(System.out::println);
       System.out.println("*****************");
 
       // update Query
@@ -78,7 +78,7 @@ public class App {
           .executeUpdate();
 
       // delete Query
-      session.createQuery("delete from users where username='M'").executeUpdate();
+      session.createQuery("delete from users where username='M'",Users.class).executeUpdate();
       session.getTransaction().commit();
     } catch (Exception e) {
       LOG.log(Level.SEVERE, "Error in Transaction");
